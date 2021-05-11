c5deb22fd74badedf3d62f7f3adc1bc091a476e7
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-05-11 22:43:36 +0900

    javascript dom test

--- freeCodeCamp/responsive-web-design/test/dom/dom_test.js
+++ freeCodeCamp/responsive-web-design/test/dom/dom_test.js
@@ -5,5 +5,21 @@ const testDom1 = function(id) {
     result.textContent = current.toLocaleString();
 }
 
+const testDom2 = function(tag) {
+    const list = document.getElementsByTagName(tag);
 
+    for (let i = 0; i < list.length; i++) {
+        console.log(list[i].href, list.item(i).className); 
+    }
+    // list.forEach(element => console.log(element));
+}
+
+const testDom3 = function(name, str) {
+    const nam = document.getElementsByName(name);
+    nam[0].value = str;
+}
+
+// メイン
 testDom1('result');
+testDom2('a');
+testDom3('test', 'Arghhhhhhhhhhh');
