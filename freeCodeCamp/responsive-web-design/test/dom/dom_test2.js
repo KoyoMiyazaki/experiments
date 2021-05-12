9ff1c043cfb6cf8baaa727df623dfc30160eba5c
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-05-12 21:43:44 +0900

    javascript dom test

--- freeCodeCamp/responsive-web-design/test/dom/dom_test2.js
+++ freeCodeCamp/responsive-web-design/test/dom/dom_test2.js
@@ -9,4 +9,22 @@ const testDom1 = function() {
     }, false); 
 }
 
+const testDom2 = function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('result_text').textContent = '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
+        document.getElementById('result_html').innerHTML = '<a href="http://www.wings.msn.to/">WINGSプロジェクト</a>';
+    }, false);
+}
+
+const testDom3 = function() {
+    document.addEventListener('DOMContentLoaded', function() {
+        let list = document.getElementById('list');
+        console.log(list.innerHTML);
+        console.log(list.textContent);
+    }, false);
+}
+
 testDom1();
+testDom2();
+testDom3();
+
