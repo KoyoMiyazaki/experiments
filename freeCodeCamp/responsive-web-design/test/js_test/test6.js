fae8e7c10f1ad0bf2d52b5bc997c3cc14f630dd7
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-05-16 11:24:33 +0900

    add freeCodeCamp file

---
+++ freeCodeCamp/responsive-web-design/test/js_test/test6.js
@@ -0,0 +1,22 @@
+function fearNotLetter(str) {
+    let result = "";
+    for(let i = 0; i < str.length - 1; i++) {
+        let [leftCharAscii, rightCharAscii] = [str[i].charCodeAt(0), str[i+1].charCodeAt(0)];
+        let missingRange = rightCharAscii - leftCharAscii;
+        if(missingRange !== 1) {
+            for(let i = 1; i < missingRange; i++) {
+                result += String.fromCharCode(leftCharAscii + i);
+            }
+        }
+    }
+    if(result === "") {
+        return undefined;
+    }
+    return result;
+}
+
+console.log(fearNotLetter("abce"));
+console.log(fearNotLetter("abcdefghjklmno"));
+console.log(fearNotLetter("stvwx"));
+console.log(fearNotLetter("bcdf"));
+console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
