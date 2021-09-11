a762c71c10e3fe4432162ea1c4f577f0d62a6ba2
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-09-11 23:55:37 +0900

    update Node.js test

---
+++ Nodejs_Test/Projects/sample2/index.js
@@ -0,0 +1,54 @@
+// let Car = class {
+//     constructor(name) {
+//         this.name = name;
+//     }
+
+//     drive() {
+//         console.log("zoom zoom ...");
+//     }
+// };
+
+// let Lamborghini = class extends Car {
+//     constructor(name) {
+//         super(name);
+//     }
+
+//     echo() {
+//         super.drive();
+//     }
+
+//     drive() {
+//         console.log(`fire ${this.name}`);
+//     }
+// };
+
+// let car = new Lamborghini("lamborghini");
+// car.echo();
+// car.drive();
+
+let util = require("util");
+
+let Car = function(name) {
+    this.name = name;
+};
+
+Car.prototype.drive = function() {
+    console.log("zoom zoom ...");
+};
+
+let Lamborghini = function(name) {
+    Lamborghini.super_.call(this, name);
+};
+util.inherits(Lamborghini, Car);
+
+Lamborghini.prototype.echo = function() {
+    Lamborghini.super_.prototype.drive.call(this);
+};
+
+Lamborghini.prototype.drive = function() {
+    console.log(`fire ${this.name}`);
+};
+
+let car = new Lamborghini("lamborghini");
+car.echo();
+car.drive();
